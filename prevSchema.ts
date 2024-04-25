9cf638cf52d2347656313002426853f3b400195d
felix_blockchainer <97702158+flockchain@users.noreply.github.com>
2024-04-25 23:06:53 +0200

    described schema and drew change idea

---
+++ prevSchema.ts
@@ -0,0 +1,34 @@
+//Old Types
+type NodeOld = {
+  index: number;
+  x: number;
+  y: number;
+  filling: number;
+  name: string;
+  color: number;
+};
+type ConnectionOld = {
+  startNodeIndex: number;
+  endNodeIndex: number;
+  degree: number;
+  sign: number;
+  color: number;
+};
+
+//New Types
+type NodeNew = {
+  index: number;
+  x: number;
+  y: number;
+  filling: number;
+  name: string;
+  color: number;
+};
+type ConnectionNew = {
+  startNodeIndex: number;
+  endNodeIndex: number;
+  degree: number;
+  sign: number;
+  color: number;
+  balances: [weight: number, treshold: number, allowance: number];
+};
